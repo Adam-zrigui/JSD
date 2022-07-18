90e5470a34d45e8bc50a34ec77af987473cb4479
adzrs <101660575+opak-exe@users.noreply.github.com>
2022-07-18 20:35:32 +0100

    Update index.js

--- dom adem/index.js
+++ dom adem/index.js
@@ -1,12 +1,17 @@
+//setup
+var items = document.getElementsByClassName("Item");
+var minBtn = document.getElementsByClassName("minus-btn");
 let plus = document.getElementsByClassName("plus-btn");
+var del = document.getElementsByClassName("delete");
+var price = document.getElementById("finalPrice");
+var lek = document.getElementsByClassName("like");
+//events
 for (let i = 0; i < plus.length; i++) {
   plus[i].addEventListener("click", function () {
     plus[i].previousElementSibling.value++;
     total();
   });
 }
-var minBtn = document.getElementsByClassName("minus-btn");
-
 for (let i = 0; i < minBtn.length; i++) {
   minBtn[i].addEventListener("click", function () {
     if (minBtn[i].nextElementSibling.value > 1) {
@@ -15,21 +20,15 @@ for (let i = 0; i < minBtn.length; i++) {
     }
   });
 }
-var price = document.getElementById("finalPrice");
-var lek = document.getElementsByClassName("like");
-
-for (let i = 0; i < lek.length; i++) {
+for (let i = 0; i < lek.length; i++) { 
   lek[i].addEventListener("click", function () {
     if (lek[i].style.color == "red") {
       lek[i].style.color = "black";
     } else lek[i].style.color = "red";
   });
 }
-var del = document.getElementsByClassName("delete");
-
 for (let i = 0; i < del.length; i++) {
   let det = del[i];
-
   det.addEventListener("click", function () {
     det.parentElement.remove();
     total();
@@ -45,4 +44,4 @@ function total() {
   }
   return (price.value = sum);
 }
-var items = document.getElementsByClassName("Item");
+
